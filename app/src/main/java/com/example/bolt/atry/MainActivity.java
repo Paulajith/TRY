f98ecb3ec7390e3290d1033ae868ec9dde4dc323
Paulajith <paulajithc@gmail.com>
2017-07-10 14:05:45 +0530

    tour guide added

--- app/src/main/java/com/example/bolt/atry/MainActivity.java
+++ app/src/main/java/com/example/bolt/atry/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.bolt.atry;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+
+import tourguide.tourguide.Overlay;
+import tourguide.tourguide.Pointer;
+import tourguide.tourguide.ToolTip;
+import tourguide.tourguide.TourGuide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnClickMe = (Button) findViewById(R.id.button);
+
+        TourGuide mTourGuideHandler = TourGuide.init(this).with(TourGuide.Technique.Click)
+                .setPointer(new Pointer())
+                .setToolTip(new ToolTip().setTitle("Welcome!").setDescription("Click on Get Started to begin..."))
+                .setOverlay(new Overlay())
+                .playOn(btnClickMe);
+
+
+
+
     }
 }
